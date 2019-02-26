e721d762425791e997d9f6a43be08945cff6e207
kenya.kakiuchi <kakiken1995@yahoo.co.jp>
2019-02-26 05:42:30 +0000

    follow, unfollow

--- app/User.php
+++ app/User.php
@@ -31,4 +31,48 @@ class User extends Authenticatable
     {
         return $this->hasMany(Micropost::class);
     }
+    
+    public function followings(){
+        return $this->belongsToMany(User::class,'user_follow','user_id','follow_id')->withTimestamps();
+    }
+    
+    public function follower(){
+        return $this->belongsToMany(User::class,'user_follow','follow_id','user_id')->withTimestamps();
+    }
+    
+    public function follow($userId){
+        //既にフォローしているかの確認
+        $exist = $this->is_following($userId);
+        //相手が自分自身でないかの確認
+        $its_me = $this->id == $userId;
+        
+        if($exist || $its_me){
+            //既にフォローしている場合
+            return false;
+        }else{    
+            //未フォローであればフォローする
+            $this->followings()->attach($userId);
+            return true;
+        }
+    }
+    public function unfollow($userId){
+        //既にフォローしているかの確認
+        $exist = $this->is_following($userId);
+        //相手が自分自身でないかの確認
+        $its_me = $this->id == $userId;
+        
+        if($exist && !$its_me){
+            //既にフォローしている場合
+            $this->followings()->detach($userId);
+            return true;
+        }else{    
+            //未フォローであればフォローする
+            return false;
+        }
+    }
+    
+    public function is_following($userId)
+    {
+        return $this->followings()->where('follow_id', $userId)->exists();
+    }
 }
